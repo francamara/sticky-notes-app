df8d07ed63df628f33b681b0386467a510e19e19
francamaradev <francamaradev@gmail.com>
2024-06-25 23:17:03 -0300

    title and better text

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -4,16 +4,18 @@ const Navbar: React.FC = () => {
   return (
     <div className="navbar bg-base-100">
       <div className="flex-none">
-        <button className="btn btn-square btn-ghost">
+        <button title="Open Menu" className="btn btn-square btn-ghost">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
         </button>
       </div>
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl" href='/'>daisyUI</a>
+        <a className="btn btn-ghost text-xl" href='/'>Stick It! Drop your ideas!</a>
       </div>
       <div className="flex-none">
-        <button className="btn btn-square btn-ghost">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
+        <button title="Share Options" className="btn btn-square btn-ghost">
+          <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" viewBox="0 0 64 64">
+            <path fill='#6699ff' d="M 9 9 L 9 14 L 9 54 L 51 54 L 56 54 L 55 42 L 51 42 L 51 49.095703 L 13 50 L 13.900391 14 L 21 14 L 21 10 L 9 9 z M 44 9 L 44 17.072266 C 29.919275 17.731863 19 23.439669 19 44 L 23 44 C 23 32.732824 29.174448 25.875825 44 25.080078 L 44 33 L 56 20.5 L 44 9 z"></path>
+          </svg>
         </button>
       </div>
     </div>
