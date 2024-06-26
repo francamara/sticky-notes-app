1b4dbf2553163d9e507229cb756d1c7f3526217d
francamaradev <francamaradev@gmail.com>
2024-06-25 23:47:23 -0300

    applied custom theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,16 +4,21 @@ const config: Config = {
   content: [
     './src/**/*.{js,ts,jsx,tsx}',
   ],
-  theme: {
-    extend: {
-      colors: {
-        primary: '#ffcc00',
-        secondary: '#ff6666',
-      },
-    },
-  },
   plugins: [
     require('daisyui'),
   ],
+  theme: {
+    extend: {
+      colors: {
+        sitckyNotesColors: {
+          'primary': '#a991f7',
+          'secondary': '#f6d860',
+          'accent': '#37cdbe',
+          'neutral': '#3d4451',
+          'base-100': '#ffffff',
+        }
+      }
+    },
+  },
 }
 export default config
